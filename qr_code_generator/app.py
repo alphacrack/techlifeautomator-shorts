01abb0eba2908a20f89b7c3fa4f201661d820680
Bishwas <jha.bishwas@gmail.com>
2023-05-24 23:57:00 +0200

    Create app.py of qr_code_generator

---
+++ qr_code_generator/app.py
@@ -0,0 +1,10 @@
+import qrcode
+
+qr = qrcode.QRCode(version=1, error_correction = qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+data = "https://www.google.com"
+qr.add_data(data)
+qr.make(fit=True)
+qr_image = qr.make_image(fill_color="black", back_color="white")
+
+
+qr_image.save("qrcode.png")
